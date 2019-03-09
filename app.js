a5317624f5a2ae73c49f307961ec43aaaa1e3f0d
zeekhoo-okta <zeekhoo@gmail.com>
2019-03-08 17:11:01 -0800

    port

--- app.js
+++ app.js
@@ -252,7 +252,7 @@ app.post('/delegate/init', authenticationRequired, (req, res) => {
 });
 
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App listening on port ${port}!`)
 });
